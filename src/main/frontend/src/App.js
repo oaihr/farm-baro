bc7f5635c9b9536e254762df205cfc3771c91bb6
oaihr <rhksgh0710@gmail.com>
2025-08-29 12:40:03 +0900

    Router 적용, Header/Footer 분리

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sale from './sale/Sale.js';
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  return(
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Sale />} />
+          <Route path="/sale/:kind/:type" element={<Sale />} />
+        </Routes>
+      <Footer />  
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/main/frontend/src/sale/Sale.js
@@ -0,0 +1,22 @@
+import './Sale.css';
+import { Link } from 'react-router-dom';
+
+function Sale(){
+
+    // const fetchMeatData = () => {
+
+    // }
+    return (
+        <div className="sales">
+
+            
+
+
+
+
+        </div>
+
+    );
+}
+
+export default Sale;
