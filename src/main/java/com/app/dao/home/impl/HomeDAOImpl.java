1ab3fa8ffa5230e8fe3676cac64f09d6fc050ae2
Dasol0303 <ekthf9135@gmail.com>
2025-09-04 11:34:27 +0900

    home_mapper.xml DB 연결 구조 이름 일부 변경

--- src/main/java/com/app/dao/home/impl/HomeDAOImpl.java
+++ src/main/java/com/app/dao/home/impl/HomeDAOImpl.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.home.HomeDAO;
-import com.app.dto.home.AuctionItem;
-import com.app.dto.home.SalesItem;
+import com.app.dto.home.AuctionItemHome;
+import com.app.dto.home.SalesItemHome;
 
 @Repository
 public class HomeDAOImpl implements HomeDAO{
@@ -17,26 +17,26 @@ public class HomeDAOImpl implements HomeDAO{
 	SqlSessionTemplate sqlSessionTemplate;
 
 	@Override
-	public List<AuctionItem> autionOnList() {
-		List<AuctionItem> homeList = sqlSessionTemplate.selectList("home_mapper.getAuctionPage");
+	public List<AuctionItemHome> getAuctionPage() {
+		List<AuctionItemHome> homeList = sqlSessionTemplate.selectList("home_mapper.getAuctionPage");
 		return homeList;
 	}
 
 	@Override
-	public List<SalesItem> getSalesPage() {
-		List<SalesItem> salesList = sqlSessionTemplate.selectList("home_mapper.getSalesPage");
+	public List<SalesItemHome> getSalesPage() {
+		List<SalesItemHome> salesList = sqlSessionTemplate.selectList("home_mapper.getSalesPage");
 		return salesList;
 	}
 
 	@Override
-	public List<AuctionItem> searchAuctions(String keyword) {
-		List<AuctionItem> auctionList = sqlSessionTemplate.selectList("home_mapper.searchAuctions", keyword);
+	public List<AuctionItemHome> searchAuctions(String keyword) {
+		List<AuctionItemHome> auctionList = sqlSessionTemplate.selectList("home_mapper.searchAuctions", keyword);
 		return auctionList;
 	}
 
 	@Override
-	public List<SalesItem> searchSales(String keyword) {
-		List<SalesItem> salesList = sqlSessionTemplate.selectList("home_mapper.searchSales", keyword);
+	public List<SalesItemHome> searchSales(String keyword) {
+		List<SalesItemHome> salesList = sqlSessionTemplate.selectList("home_mapper.searchSales", keyword);
 		return salesList;
 	}
 
